7834ba035726b7421b1f3e0e42161e7e518c5d22
REV623 <90751501+REV623@users.noreply.github.com>
2022-08-04 14:08:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const gendericon = document.querySelector("#span-gender-icon");
+const name = document.querySelector("#p-name");
+const div1 = document.querySelector("#div-user-card");
+const pic = document.querySelector("#img-profile");
+const email = document.querySelector("#p-email");
+const load = document.querySelector("#div-loading-card");
+const btnrandom = document.querySelector("#btn-random");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  const gender = resp.data.results[0].gender;
+  if (gender == "male") gendericon.innerText = "👨";
+  else if (gender == "female") gendericon.innerText = "👩";
+  name.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  pic.src = resp.data.results[0].picture.large;
+  email.innerText = resp.data.results[0].email;
 }
+
+callApi();
+load.disabled = true;
+
+btnrandom.onclick = async () => {
+  callApi();
+};
+
